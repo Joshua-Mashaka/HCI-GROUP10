1e3ecfc040064a955dae93fcd7ca0b8c7c4639f0
Joshua-Mashaka <bed-com-13-18@cc.ac.mw>
2021-08-06 20:04:55 -0700

    make it better

--- ussd.php
+++ ussd.php
@@ -6,10 +6,10 @@ $phoneNumber = $_POST["phoneNumber"];
 $text = $_POST["text"];
 
 if ($text == "") {
-    $response = "1. Balance Enquiry\n"; 
-    $response = "2. Send Money\n";
-    $response = "3. Make Payment\n";
-    $response = "4. Buy Airtime\n";
+    $response = "1. Balance Enquiry \n"; 
+    $response = "2. Send Money \n";
+    $response = "3. Make Payment \n";
+    $response = "4. Buy Airtime \n";
     $response = "5. Other Service";
 }elseif ($text == "2") {
     $response = "1. To Account\n";
