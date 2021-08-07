f53513df1844e9ce697eb498d5d4cb25128c7b36
Joshua-Mashaka <bed-com-13-18@cc.ac.mw>
2021-08-06 20:49:07 -0700

    make it better

--- index.php
+++ index.php
@@ -19,36 +19,7 @@ if ($text == "") {
     $response = "4. To Other Banks";
     
 }
-<?php
 
-$sessionId = $_POST["sessionId"]; 
-$serviceCode = $_POST["serviceCode"];
-$phoneNumber = $_POST["phoneNumber"];
-$text = $_POST["text"];
-
-if ($text == "") {
-    $response1 = "CON 1. Balance Enquiry \n";
-    $response2 = "2. Send Money \n";
-    $response3 = "3. Make Payment \n";
-    $response4 = "4. Buy Airtime \n";
-    $response5 = "5. Other Service";
-
-}elseif ($text == "2") {
-    $response = "CON 1. To Account\n";
-    $response = "2. To Saved Accounts";
-    $response = "3. To Phone Number";
-    $response = "END 4. To Other Banks";
-    
-}
-
-header('content-type: text/plain');
-echo $response1;
-echo $response2;
-echo $response3;
-echo $response4;
-echo $response5;
-
-?>
 header('content-type: text/plain');
 echo $response1;
 echo $response2;
