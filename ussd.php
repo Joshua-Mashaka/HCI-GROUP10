5a242bad487d5319d1d27f93620ff536fe00f4a2
Wongani Msumba <bed-com-12-17@cc.ac.mw>
2021-08-11 17:53:12 +0100

    connecting to database

--- ussd.php
+++ ussd.php
@@ -1,24 +1,74 @@
 <?php
 
+// $conn = 'msql:dbname=ussdapp; host=127.0.0.1;';
+// $user = 'root';
+// $password = '';
+
+// try{
+//     $db = new PDO($conn, $user, $password);
+//     echo("connected successfully");
+// }
+
+// catch(PDOException $e) {
+//     echo("PDO error occured");
+// }
+
+// catch(Exception $e) {
+//     echo("PDO error occured");
+// }
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+
 $sessionId = $_POST["sessionId"]; 
 $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
 $text = $_POST["text"];
 
-if ($text == "") {
-    $response1 = "CON 1. Balance Enquiry \n";
-    $response2 = "2. Send Money \n";
-    $response3 = "3. Make Payment \n";
-    $response4 = "4. Buy Airtime \n";
-    $response5 = "5. Other Service";
+// $sql = "SELECT id, firstname, lastname FROM MyGuests";
+// $result = $conn->query($sql);
 
-}elseif ($text == "2") {
-    $response = "CON 1. To Account\n";
-    $response = "2. To Saved Accounts";
-    $response = "3. To Phone Number";
-    $response = "END 4. To Other Banks";
+// if ($result->num_rows > 0) {
+//   // output data of each row
+//   while($row = $result->fetch_assoc()) {
+//     echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+//   }
+// } else {
+//   echo "0 results";
+// }
+
+// if ($text == "") {
+//     $response1 = "CON 1. Balance Enquiry \n";
+//     $response2 = "2. Send Money \n";
+//     $response3 = "3. Make Payment \n";
+//     $response4 = "4. Buy Airtime \n";
+//     $response5 = "5. Other Service";
+
+// }elseif ($text == "2") {
+//     $response = "CON 1. To Account\n";
+//     $response = "2. To Saved Accounts";
+//     $response = "3. To Phone Number";
+//     $response = "END 4. To Other Banks";
     
+// }
+
+if ($text = "") {
+    $response = "enter user name";
+
 }
+    $sql = "INSERT INTO MyGuests (userName)
+VALUES ($text)";
 
 header('content-type: text/plain');
 echo $response1;
